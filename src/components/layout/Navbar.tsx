c9711ab745a61e621ffb752c2301468dc96f3e3e
John Kaburu <32298219+blackninja81@users.noreply.github.com>
2024-03-18 16:31:20 +0300

    [BUG] fixed errors

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,6 +1,4 @@
-import React from 'react'
 import { GithubOutlined } from '@ant-design/icons'
-import logo from '../../assets/images/logo.png'
 const Navbar = () => {
   return (
     <div className='navbar-container'>
