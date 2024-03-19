bd4deabde4741ce296a772aab758ef7a254ef42e
John Kaburu <32298219+blackninja81@users.noreply.github.com>
2024-03-19 18:19:59 +0300

    [FEATURE] converted images to webp

--- src/constants/constants.tsx
+++ src/constants/constants.tsx
@@ -1,191 +1,191 @@
 export const Resources = {
     tools: {
       divider: {
-        image: "/assets/images/screenshots/screenshot1.png",
+        image: "/assets/images/screenshots/screenshot1.webp",
         label: "Shape Divider",
         desciption:"create custom shapes to divide sections ",
         link: "https://www.shapedivider.app/",
       },
       animejs: {
-        image: "/assets/images/screenshots/screenshot2.png",
+        image: "/assets/images/screenshots/screenshot2.webp",
         label: "AnimeJs",
         desciption:"Create animations for js frameworks",
         link: "https://animejs.com/",
       },
       Omatsuri: {
-        image: "/assets/images/screenshots/screenshot3.png",
+        image: "/assets/images/screenshots/screenshot3.webp",
         label: "Omatsuri",
         desciption:"Open source browser tools for everyday use",
         link: "https://omatsuri.app/",
       },
       shadows: {
-        image: "/assets/images/screenshots/screenshot4.png",
+        image: "/assets/images/screenshots/screenshot4.webp",
         label: "Box Shadows",
         desciption:"Collection of css shadows",
         link: "https://getcssscan.com/css-box-shadow-examples",
       }
       ,
       loaders: {
-        image: "/assets/images/screenshots/screenshot5.png",
+        image: "/assets/images/screenshots/screenshot5.webp",
         label: "Loading Animations",
         desciption:"loading animations and preloaders",
         link: "https://loading.io/",
       }
       ,
       glassmophism: {
-        image: "/assets/images/screenshots/screenshot6.png",
+        image: "/assets/images/screenshots/screenshot6.webp",
         label: "GlassMophism",
         desciption:"glass background effect",
         link: "https://css.glass/",
       }
       ,
       colors: {
-        image: "/assets/images/screenshots/screenshot7.png",
+        image: "/assets/images/screenshots/screenshot7.webp",
         label: "Color Palettes",
         desciption:"Color palettes from different sites",
         link: "https://materialui.co/",
       }
       ,
       undraw: {
-        image: "/assets/images/screenshots/screenshot8.png",
+        image: "/assets/images/screenshots/screenshot8.webp",
         label: "Un Draw",
         desciption:"Multiple web illustrations",
         link: "https://undraw.co/",
       }
       ,
       icons8: {
-        image: "/assets/images/screenshots/screenshot9.png",
+        image: "/assets/images/screenshots/screenshot9.webp",
         label: "Icons8",
         desciption:"Curated graphics, design apps, and AI tools",
         link: "https://icons8.com/",
       },
       reactreveal: {
-        image: "/assets/images/screenshots/screenshot10.png",
+        image: "/assets/images/screenshots/screenshot10.webp",
         label: "React Reveal",
         desciption:"Animated components for React apps",
         link: "https://react-awesome-reveal.morello.dev/",
       }
       ,
       lottifiles: {
-        image: "/assets/images/screenshots/screenshot11.png",
+        image: "/assets/images/screenshots/screenshot11.webp",
         label: "Lotti Files",
         desciption:"Lightweight, scalable animations",
         link: "https://lottiefiles.com/",
       }
       ,
       mockaroo: {
-        image: "/assets/images/screenshots/screenshot12.png",
+        image: "/assets/images/screenshots/screenshot12.webp",
         label: "Mockaroo",
         desciption:"Create Mock data for you app",
         link: "https://mockaroo.com/",
       }
       ,
       realtimecolors: {
-        image: "/assets/images/screenshots/screenshot14.png",
+        image: "/assets/images/screenshots/screenshot14.webp",
         label: "Realtime Colors",
         desciption:"visualize colors and fonts on a real site",
         link: "https://www.realtimecolors.com/?colors=050315-fbfbfe-2f27ce-dedcff-433bff&fonts=Poppins-Poppins",
       }
       ,
       coolors: {
-        image: "/assets/images/screenshots/screenshot13.png",
+        image: "/assets/images/screenshots/screenshot13.webp",
         label: "Coolors",
         desciption:"color palettes generator",
         link: "https://coolors.co/",
       }
       ,
       haikei: {
-        image: "/assets/images/screenshots/screenshot16.png",
+        image: "/assets/images/screenshots/screenshot16.webp",
         label: "Haikei",
         desciption:"Generate shapes and backgrounds",
         link: "https://app.haikei.app/",
       }
       ,
       freefrontend: {
-        image: "/assets/images/screenshots/screenshot17.png",
+        image: "/assets/images/screenshots/screenshot17.webp",
         label: "Free Frontend",
         desciption:"Multiple css components",
         link: "https://freefrontend.com/",
       }
       ,
       neumorphism: {
-        image: "/assets/images/screenshots/screenshot18.png",
+        image: "/assets/images/screenshots/screenshot18.webp",
         label: "Neumorphism",
         desciption:"Generate Soft-UI CSS code",
         link: "https://neumorphism.io/#e0e0e0",
       }
       ,
       reactmail: {
-        image: "/assets/images/screenshots/screenshot19.png",
+        image: "/assets/images/screenshots/screenshot19.webp",
         label: "React Mail",
         desciption:"Collection of components for creating emails",
         link: "https://react.email/",
       }
       ,
       shadcn: {
-        image: "/assets/images/screenshots/screenshot20.png",
+        image: "/assets/images/screenshots/screenshot20.webp",
         label: "Shadcn",
         desciption:"Beautifully designed components",
         link: "https://ui.shadcn.com/",
       }
       ,
       fffuel: {
-        image: "/assets/images/screenshots/screenshot21.png",
+        image: "/assets/images/screenshots/screenshot21.webp",
         label: "fffuel",
         desciption:"collection of color tools and SVG generators",
         link: "https://fffuel.co/",
       }
       ,
       Spline: {
-        image: "/assets/images/screenshots/screenshot22.png",
+        image: "/assets/images/screenshots/screenshot22.webp",
         label: "Spline",
         desciption:"Design and collaborate in 3D",
         link: "https://spline.design",
       }
       ,
       driverjs: {
-        image: "/assets/images/screenshots/screenshot23.png",
+        image: "/assets/images/screenshots/screenshot23.webp",
         label: "DriverJs",
         desciption:"Product tours, highlights and contextual help",
         link: "https://driverjs.com/",
       }
       ,
       cssbuttons: {
-        image: "/assets/images/screenshots/screenshot24.png",
+        image: "/assets/images/screenshots/screenshot24.webp",
         label: "css Buttons",
         desciption:"Multiple buttons designed in css",
         link: "https://cssbuttons.app/",
       }
       ,
       splide: {
-        image: "/assets/images/screenshots/screenshot25.png",
+        image: "/assets/images/screenshots/screenshot25.webp",
         label: "Splide",
         desciption:"A lightweight, flexible, accessible carousel",
         link: "https://splidejs.com/",
       }
       ,
       removebackground: {
-        image: "/assets/images/screenshots/screenshot26.png",
+        image: "/assets/images/screenshots/screenshot26.webp",
         label: "Remove Background",
         desciption:"Remove Background From Image",
         link: "https://removal.ai/",
       }
       ,
       tinypng: {
-        image: "/assets/images/screenshots/screenshot27.png",
+        image: "/assets/images/screenshots/screenshot27.webp",
         label: "Tiny png",
         desciption:"Smart WebP, PNG and JPEG Compression",
         link: "https://tinypng.com/",
       }
       ,
       iconify: {
-        image: "/assets/images/screenshots/screenshot28.png",
+        image: "/assets/images/screenshots/screenshot28.webp",
         label: "Iconify",
         desciption:"Over 200,000 open source vector icons",
         link: "https://iconify.design/",
       },
       gradient: {
-        image: "/assets/images/screenshots/screenshot29.png",
+        image: "/assets/images/screenshots/screenshot29.webp",
         label: "Gradient",
         desciption:"Make backgrounf gradients",
         link: "https://cssgradient.io/",
@@ -313,32 +313,32 @@ export const Resources = {
     },
     inspiration: {
         awwwards: {
-            image: "/assets/images/screenshots/screenshot34.png",
+            image: "/assets/images/screenshots/screenshot34.webp",
             label: "Awwwards",
             link: "https://www.awwwards.com/",
           },
           darkmode: {
-            image: "/assets/images/screenshots/screenshot35.png",
+            image: "/assets/images/screenshots/screenshot35.webp",
             label: "Dark Mode Design",
             link: "https://www.darkmodedesign.com/",
           },
           godly: {
-            image: "/assets/images/screenshots/screenshot36.png",
+            image: "/assets/images/screenshots/screenshot36.webp",
             label: "godly",
             link: "https://godly.website/",
           }, 
           refero: {
-            image: "/assets/images/screenshots/screenshot37.png",
+            image: "/assets/images/screenshots/screenshot37.webp",
             label: "Refero",
             link: "https://refero.design/",
           }, 
           stacksorted: {
-            image: "/assets/images/screenshots/screenshot38.png",
+            image: "/assets/images/screenshots/screenshot38.webp",
             label: "Stack Sorted",
             link: "https://stacksorted.com/buttons",
           }, 
           startupwebsites: {
-            image: "/assets/images/screenshots/screenshot39.png",
+            image: "/assets/images/screenshots/screenshot39.webp",
             label: "Startup Websites",
             link: "https://www.startupwebsites.co/",
           }, 
@@ -346,31 +346,31 @@ export const Resources = {
     },
     fonts: {
       fontjoy: {
-        image: "/assets/images/screenshots/screenshot15.png",
+        image: "/assets/images/screenshots/screenshot15.webp",
         label: "Font Joy",
         desciption:"Generate font combinations",
         link: "https://fontjoy.com/",
       },
       googlefonts: {
-        image: "/assets/images/screenshots/screenshot30.png",
+        image: "/assets/images/screenshots/screenshot30.webp",
         label: "Google Fonts",
         desciption:"Collection of fonts by google",
         link: "https://fonts.google.com/",
       } ,
       dafont: {
-        image: "/assets/images/screenshots/screenshot31.png",
+        image: "/assets/images/screenshots/screenshot31.webp",
         label: "Dafont",
         desciption:"Collection of many fonts",
         link: "https://www.dafont.com/",
       },
       fontshare: {
-        image: "/assets/images/screenshots/screenshot32.png",
+        image: "/assets/images/screenshots/screenshot32.webp",
         label: "Fontshare",
         desciption:"Collection of fonts",
         link: "https://www.fontshare.com/",
       },
       uncut: {
-        image: "/assets/images/screenshots/screenshot33.png",
+        image: "/assets/images/screenshots/screenshot33.webp",
         label: "Uncut",
         desciption:"Collection of fonts",
         link: "https://uncut.wtf/",
